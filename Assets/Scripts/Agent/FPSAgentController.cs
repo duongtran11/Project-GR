dc690c5e4866cb02b354f1350b1ffb19209fdcc3
duongtran11 <goldend2406@gmail.com>
2025-12-06 15:12:55 +0700

    Done creating FPS agent controller

--- Assets/Scripts/Agent/FPSAgentController.cs
+++ Assets/Scripts/Agent/FPSAgentController.cs
@@ -9,7 +9,7 @@ public class FPSAgentController : AgentController
 
     private void HandleMovement()
     {
-        _moveDirection = new Vector3(_moveInput.x, 0, _moveInput.y);
+        _moveDirection = transform.rotation * new Vector3(_moveInput.x, 0, _moveInput.y);
         transform.position += _walkingSpeed * Time.deltaTime * _moveDirection;
     }
 }
--- Assets/Scripts/Camera/FPSCameraRig.cs
+++ Assets/Scripts/Camera/FPSCameraRig.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class FPSCameraRig : MonoBehaviour, IInputAxisOwner
 {
-
+    [SerializeField]
+    private FPSAgentController _agentController;
     public InputAxis HorizontalLook = new() { Range = new Vector2(-180f, 180f), Wrap = true, Recentering = InputAxis.RecenteringSettings.Default };
     public InputAxis VerticalLook = new() { Range = new Vector2(-60f, 60f), Recentering = InputAxis.RecenteringSettings.Default };
 
@@ -26,6 +27,7 @@ public class FPSCameraRig : MonoBehaviour, IInputAxisOwner
 
     void Update()
     {
-
+        _agentController.transform.rotation = Quaternion.Euler(0f, HorizontalLook.Value, 0f);
+        transform.localRotation = Quaternion.Euler(VerticalLook.Value, 0f, 0f);
     }
 }
