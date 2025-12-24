5b875c58f2e93cf6591d3b3f6bfb275fea8aa907
duongtran11 <goldend2406@gmail.com>
2025-12-25 00:02:20 +0700

    Create a patrol enemy using navmesh agent

--- Assets/Scripts/Agent/EnemyController.cs
+++ Assets/Scripts/Agent/EnemyController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -5,7 +6,11 @@ public class EnemyController : MonoBehaviour, IDamageable
 {
     private Animator _anim;
     private NavMeshAgent _navMeshAgent;
-    private AgentController _player;
+    [SerializeField] private AgentController _player;
+    [SerializeField] private List<Transform> _waypointList = new();
+    private Vector3 _currentDestination;
+    private int _currentIndex;
+    [SerializeField] private float _minReachDistance = 0.1f;
     public string Name { get; }
     public float Health { get; set; }
 
@@ -13,15 +18,48 @@ public class EnemyController : MonoBehaviour, IDamageable
     {
         _anim = GetComponent<Animator>();
         _navMeshAgent = GetComponent<NavMeshAgent>();
-        _player = PlayerManager.Instance.Player;
+        _currentDestination = _waypointList[_currentIndex].position;
+        _navMeshAgent.destination = _currentDestination;
+        // _player = PlayerManager.Instance.Player;
     }
     void Update()
     {
-        if (_player != null)
+        // if (_player != null)
+        // {
+        //     FollowPlayer();
+        // }
+        Patrol();
+    }
+
+    private void Patrol()
+    {
+        var distanceToDestination = (_currentDestination - transform.position).magnitude;
+        if (distanceToDestination <= _minReachDistance)
         {
-            FollowPlayer();
+            if (_currentIndex < _waypointList.Count - 1)
+            {
+                _currentIndex++;
+                _currentDestination = _waypointList[_currentIndex].position;
+                _navMeshAgent.destination = _currentDestination;
+            }
+            else
+            {
+                ResetPath();
+            }
         }
     }
+
+    private void ResetPath()
+    {
+        _currentIndex = 0;
+        for (int i = 0; i < _waypointList.Count / 2; i++)
+        {
+            var temp = _waypointList[i].position;
+            _waypointList[i].position = _waypointList[_waypointList.Count - 1 - i].position;
+            _waypointList[_waypointList.Count - 1 - i].position = temp;
+        }
+    }
+
     void FollowPlayer()
     {
         _navMeshAgent.destination = _player.transform.position;
